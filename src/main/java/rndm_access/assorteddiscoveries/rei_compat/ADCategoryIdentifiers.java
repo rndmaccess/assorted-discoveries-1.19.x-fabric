6a40194f5db98fe11d2bfa7447ad533acd06c8b8
rndmaccess <rndmaccessgaming@gmail.com>
2022-09-10 15:29:11 -0600

    Replaced support for rei with recipe advancements and fixed some bugs.

--- src/main/java/rndm_access/assorteddiscoveries/rei_compat/ADCategoryIdentifiers.java
+++
@@ -1,9 +0,0 @@
-package rndm_access.assorteddiscoveries.rei_compat;
-
-import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import rndm_access.assorteddiscoveries.ADReference;
-import rndm_access.assorteddiscoveries.rei_compat.client.display.ADWoodcuttingDisplay;
-
-public class ADCategoryIdentifiers {
-    public static final CategoryIdentifier<ADWoodcuttingDisplay> WOOD_CUTTING = CategoryIdentifier.of(ADReference.MOD_ID, "plugins/woodcutter");
-}
