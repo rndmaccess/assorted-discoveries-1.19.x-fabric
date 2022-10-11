cfba101ae2875cee90d4555075f2cbe818ec1e8a
rndmaccess <rndmaccessgaming@gmail.com>
2022-10-10 22:19:11 -0600

    Cleaned up some code.

--- src/main/java/rndm_access/assorteddiscoveries/common/block/ADPlanterBoxBlock.java
+++ src/main/java/rndm_access/assorteddiscoveries/common/block/ADPlanterBoxBlock.java
@@ -1,7 +1,10 @@
 package rndm_access.assorteddiscoveries.common.block;
 
 import com.google.common.collect.ImmutableList;
-import net.minecraft.block.*;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
@@ -48,20 +51,19 @@ public class ADPlanterBoxBlock extends Block {
 
         // Add every subset to the map which covers every possible shape for every state these blocks.
         for (int i = 0; i < power_set_size; i++) {
-            List<Boolean> isBorderOpen = new ArrayList<>(4);
+            ArrayList<Boolean> edges = new ArrayList<>(4);
             VoxelShape tempBorderShape = VoxelShapes.empty();
 
             for (int j = 0; j < setSize; j++) {
-                // If true there is a border here on the planter box.
-                if ((i >> j) % 2 == 1) {
-                    isBorderOpen.add(false);
+                // If this bit is on we know there has to be an edge on the planter box.
+                if (((i >> j) & 0x01) == 1) {
+                    edges.add(false);
                     tempBorderShape = VoxelShapes.union(tempBorderShape, borderShapes.get(j));
-                }
-                else {
-                    isBorderOpen.add(true);
+                } else {
+                    edges.add(true);
                 }
             }
-            map.put(isBorderOpen, VoxelShapes.union(tempBorderShape, bottomShape));
+            map.put(edges, VoxelShapes.union(tempBorderShape, bottomShape));
         }
         return map;
     }
