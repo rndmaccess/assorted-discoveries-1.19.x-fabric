4b36e8c0acebb3ae56388ee9900d24c885a12b9e
rndmaccess <rndm.access.dev@gmail.com>
2023-03-13 10:08:25 -0600

    Removed the jungle snare from the cuttout list.

--- src/main/java/rndm_access/assorteddiscoveries/client/AssortedDiscoveriesClient.java
+++ src/main/java/rndm_access/assorteddiscoveries/client/AssortedDiscoveriesClient.java
@@ -170,8 +170,7 @@ public class AssortedDiscoveriesClient implements ClientModInitializer {
                 ADBlocks.ENDER_GRASS, ADBlocks.ICICLE, ADBlocks.CATTAIL, ADBlocks.POTTED_PURPLE_MUSHROOM,
                 ADBlocks.BLOOD_KELP, ADBlocks.BLOOD_KELP_PLANT, ADBlocks.MANGROVE_ROPE_LADDER, ADBlocks.WEEPING_HEART,
                 ADBlocks.MAROON_CAMPFIRE, ADBlocks.MAROON_TORCH, ADBlocks.MAROON_WALL_TORCH, ADBlocks.MAROON_LANTERN,
-                ADBlocks.DRIED_BAMBOO_FENCE_GATE, ADBlocks.FRESH_BAMBOO_FENCE_GATE,
-                ADBlocks.JUNGLE_SNARE, ADBlocks.JUNGLE_SNARE_PLANT);
+                ADBlocks.DRIED_BAMBOO_FENCE_GATE, ADBlocks.FRESH_BAMBOO_FENCE_GATE);
 
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
                 ADBlocks.ENDERMAN_PLUSH, ADBlocks.GRASS_SLAB);
